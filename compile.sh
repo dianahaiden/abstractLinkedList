77343845e9bf1c058c01eecb0c86c280663ef4d7
dianahaiden <63987203+dianahaiden@users.noreply.github.com>
2021-03-02 18:13:15 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 
-javac -Xlint -d bin src/cs1302/genlist/Node.java
-javac -Xlint -cp bin:lib/genlistadt.jar -d bin src/cs1302/genlist/LinkedGenList.java
-javac -Xlint -d bin -cp bin:lib/genlistadt.jar src/cs1302/genlist/ListIterator.java
-javac -Xlint -d bin -cp bin:lib/genlistadt.jar src/cs1302/genlist/LinkedGenListTest.java
-java -cp bin:lib/genlistadt.jar cs1302.genlist.LinkedGenListTest
+javac -Xlint -d bin src/genlist/Node.java
+javac -Xlint -cp bin:lib/genlistadt.jar -d bin src/genlist/LinkedGenList.java
+javac -Xlint -d bin -cp bin:lib/genlistadt.jar src/genlist/ListIterator.java
+javac -Xlint -d bin -cp bin:lib/genlistadt.jar src/genlist/LinkedGenListTest.java
+java -cp bin:lib/genlistadt.jar genlist.LinkedGenListTest
